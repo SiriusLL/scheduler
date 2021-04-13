860b95d1ba81a7bfeba0cf6c6bdf11909a7faafb
SiriusLL <sirius.love.light@gmail.com>
2021-04-12 21:21:00 -0600

    button, dayList, dayListItem components

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+//import classnames from "classnames";
+//import "components/DayListItem.scss";
+
+export default function DayList(props) {
+  console.log("props:", props.days);
+  const day = props.days.map((day) => {
+    console.log("day", day);
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+  console.log("return", day);
+  return <ul>{day}</ul>;
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  console.log("propssss", props);
+  const formatSpots = function () {
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
+    } else if (props.spots === 0) {
+      return `no spots remaining`;
+    } else if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+  };
+  const listItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+  return (
+    <li className={listItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
